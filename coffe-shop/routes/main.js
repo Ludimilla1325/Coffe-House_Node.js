aa13db070a8960a9e9cc294f45a11aa2aadefb47
Ludimilla Krautzuk <ludimilla1325@gmail.com>
2021-01-22 12:15:59 -0300

    Rendering with the global.json

--- coffe-shop/routes/main.js
+++ coffe-shop/routes/main.js
@@ -2,13 +2,11 @@ const express = require ('express')
 const router = express.Router()
 
 router.get('/',(req, res, next) =>{
-    const data = req.context //the context contain the global.json
-    res.render('home',data)
+    res.render('home',req.context) //context contain the global.json
 })
 
     router.get('/blog',(req, res, next) => {
-        const data = req.context
-        res.render('../views/blog',data)
+        res.render('../views/blog',req.context)
     })
 
 
