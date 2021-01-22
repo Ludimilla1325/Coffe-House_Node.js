5436eb0a55eb3de0be9ae068ae5789f2711cb14b
Ludimilla Krautzuk <ludimilla1325@gmail.com>
2021-01-22 11:00:25 -0300

    Using global.json

--- coffe-shop/routes/main.js
+++ coffe-shop/routes/main.js
@@ -2,19 +2,12 @@ const express = require ('express')
 const router = express.Router()
 
 router.get('/',(req, res, next) =>{
-    const data = {
-        title:" Ludi's Coffe House",
-        description:"Welcome to our Coffe house! Enjoy it"
-    }
-
+    const data = req.context //the context contain the global.json
     res.render('home',data)
 })
 
     router.get('/blog',(req, res, next) => {
-        const data = {
-            title:"Ludi's Coffe House",
-            description:"Welcome to our Coffe house! Enjoy it"
-        }
+        const data = req.context
         res.render('../views/blog',data)
     })
 
